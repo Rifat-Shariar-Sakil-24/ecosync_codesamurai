432d25d3d494f6b05fe845f83b958bc5cf77ba0d
S R Rayhan <shafikulrahman66@gmail.com>
2024-05-01 17:12:12 +0600

    Landing Page Created

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'antd/dist/reset.css'
+import { Button, Space } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import '../css/landingPage.css';
+
+
+const LandingPage = () => {
+    
+    return (
+        <div>
+            <div className="container">
+                <div className="left-container">
+                    <div className="left-containers">
+                        <h1>DNCC Waste Management</h1>
+                        <img src='../../public/Images/dustbin.png'></img>
+                        <div className="button-container">
+                            <Space>
+                                <Button type='primary' size='large' className='login-button'>Login</Button>
+                            </Space>
+                            
+                        </div> 
+                    </div>
+                </div>
+                <div className="right-container">
+                    <img src='../../public/Images/LandingPageImage.png' alt='image'></img>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
